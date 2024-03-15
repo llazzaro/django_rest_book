3ba579a3b1bd34e4cee145fd5b2ea9b8698c6d80
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2024-03-15 21:00:44 +0100

    refactor(movies): optimize query performance and reduce memory usage

--- src/movies/urls.py
+++ src/movies/urls.py
@@ -1,12 +1,12 @@
 # urls.py
 from django.urls import path
 
-from movies.api import MovieAPIView
+from movies.api import MovieDetailAPIView, MovieListCreateAPIView
 
 app_name = "movies"  # Define the application namespace
 
 
 urlpatterns = [
-    path("movies/", MovieAPIView.as_view(), name="movie-api"),
-    path("movies/<int:pk>/", MovieAPIView.as_view(), name="movie-api-detail"),
+    path('movies/', MovieListCreateAPIView.as_view(), name='movie-api'),
+    path('movies/<int:pk>/', MovieDetailAPIView.as_view(), name='movie-api-detail'),
 ]
